1e8d5cf900ede44d0471b5bcd02ba53c73dbad5c
pepelyaeva <pepelyaevaks@gmail.com>
2015-10-22 21:03:59 +0600

    Individual work.

---
+++ Pepelyaeva Kseniya/Individual work/Ippodrom/ASP.NET/shared/script/treeviewIcons.js
@@ -0,0 +1,6 @@
+﻿var trElem;
+for (i = 0; i < 4; i++) {
+    trElem = document.getElementById("TreeView1n6Nodes").getElementsByTagName("table")[i].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0];
+    trElem.getElementsByTagName("td")[1].style.backgroundImage = "url('../../App_Themes/Spring/Images/TreeView" + i + ".png')";
+    trElem.getElementsByTagName("td")[1].style.backgroundPosition = "center";
+}
