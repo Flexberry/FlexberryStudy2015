7a888f7e034cde0400afee825201ecb8dca4fa44
pepelyaeva <pepelyaevaks@gmail.com>
2015-11-08 19:57:46 +0600

    Correction of the remarks.

--- Pepelyaeva Kseniya/Individual work/Ippodrom/ASP.NET/shared/script/treeviewIcons.js
+++ Pepelyaeva Kseniya/Individual work/Ippodrom/ASP.NET/shared/script/treeviewIcons.js
@@ -1,6 +1,13 @@
-﻿var trElem;
-for (i = 0; i < 4; i++) {
-    trElem = document.getElementById("TreeView1n6Nodes").getElementsByTagName("table")[i].getElementsByTagName("tbody")[0].getElementsByTagName("tr")[0];
-    trElem.getElementsByTagName("td")[1].style.backgroundImage = "url('../../App_Themes/Spring/Images/TreeView" + i + ".png')";
-    trElem.getElementsByTagName("td")[1].style.backgroundPosition = "center";
+﻿$(document).ready(function () {
+    setTreeViewIcons();
+});
+
+// Добавить иконки в дерево навигации.
+function setTreeViewIcons() {
+    var $trElem;
+    for (i = 0; i < 4; i++) {
+        $trElem = $("#TreeView1n6Nodes").find("table:eq(" + i + ")").find("tbody:first").find("tr:first").find("td:eq(1)");
+        $trElem.css("background-image", "url('../../App_Themes/Spring/Images/TreeView" + i + ".png')");
+        $trElem.css("background-position", "center");
+    }
 }
